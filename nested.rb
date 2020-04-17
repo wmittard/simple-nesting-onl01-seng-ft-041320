02364549137495be6afc27239966738d83fe617e
William Mittard <billymittard@ymail.com>
2020-04-17 20:19:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,10 +110,9 @@ def changing_alan
      alans_new_info = "GUI"
     
      programmer_hash[:alan_kay][:known_for] = "GUI" 
-     programmer_hash
-     
-     
-end
+     programmer_hash[:alan_kay][:known_for]
+    
+     end
 
 def adding_to_dennis
 	programmer_hash = 
