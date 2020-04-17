fa2c9993a6a96db8425f9a42e2ea27770fb4bf93
William Mittard <billymittard@ymail.com>
2020-04-17 20:14:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,6 +108,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      programmer_hash[:alan_kay]
+     [:known_for]
      
      
 end
